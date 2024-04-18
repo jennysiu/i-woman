58535f14431d4a376fa2ac5fef6bb96d8c74b07a
jennysiu <jenny.siu79@gmail.com>
2024-04-18 20:31:08 +0100

    page context added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,53 @@
 import { useTheme } from "../contexts/ThemeContext";
 
+// internal imports
+
 const Home = () => {
   const { theme } = useTheme();
 
   return (
     <>
       <div className={theme === "dark" ? "dark-theme" : ""}>
-        <h1>Hello</h1>
+        <header>
+          <h1>i-woman</h1>
+          <p>
+            The woman's business hub creates an inclusive culture to
+            successfully harness entrepreneur's potential to truly transform
+            society.
+          </p>
+          <button>Join Us</button>
+        </header>
+        <section>
+          <div>
+            <h3>Unite</h3>
+            <p>
+              Aliqua magna aute officia duis proident enim quis culpa qui quis
+              amet ea.
+            </p>
+            <button>Join Us</button>
+          </div>
+          <div>
+            <h3>Network</h3>
+            <p>
+              Aliqua magna aute officia duis proident enim quis culpa qui quis
+              amet ea.
+            </p>
+            <button>Join Us</button>
+          </div>
+          <div>
+            <h3>Progress</h3>
+            <p>
+              Aliqua magna aute officia duis proident enim quis culpa qui quis
+              amet ea.
+            </p>
+            <button>Join Us</button>
+          </div>
+        </section>
+        <section>
+          <h2>Join Our Groups</h2>
+          <p>Explore our groups to see what you can learn and contribute.</p>
+          <button>Explore Groups</button>
+        </section>
       </div>
     </>
   );
